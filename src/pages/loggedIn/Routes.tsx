c162540675fa4847c8f68021bd9bebf7fecda93c
Terminal-Adawe <bede.abbe91@gmail.com>
2026-01-29 08:39:58 +0000

    changes

--- src/pages/loggedIn/Routes.tsx
+++ src/pages/loggedIn/Routes.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ApplicationContext from "../../../resources/providers/ApplicationContext";
 import HomePage from "./Home";
 import LoadingOverlay from "../../components/LoadingOverlay";
@@ -18,7 +18,8 @@ const CustomRoutes: React.FC = () => {
                 {/* <Route path="*" element={<NotFoundPage />} /> */}
                 {/* <Route path={ROUTES.HOMEPAGE_ROUTE} element={<HomePage />} /> */}
                 <Route path='/home' element={<HomePage />} />
-                {/* <Route path="*" element={<Navigate replace to="/" />} /> */}
+                <Route path="/" element={<Navigate replace to="/learn/home" />} />
+                <Route path="*" element={<Navigate replace to="/" />} />
             </Routes>
             {/* <Invoice showModal={showInvoice} handleClose={handleShowInvoiceModalClose} invoice={selectedInvoice} />
             <NotififcationModal notificationProps={appContext!.notificationProps!} />
